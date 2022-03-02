06a0143b1605a845776b05d8f3156d1d9a227eac
AutMai <shuber0802@gmail.com>
2022-03-02 19:12:11 +0100

    added wordAutoPdf add-in

---
+++ word/WordAutoPdf/WordAutoPdf/ThisAddIn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+
+namespace WordAutoPdf
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.DocumentBeforeSave += new ApplicationEvents4_DocumentBeforeSaveEventHandler(Application_DocumentBeforeSave);
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+        
+        
+
+        void Application_DocumentBeforeSave(Microsoft.Office.Interop.Word.Document Doc, ref bool SaveAsUI, ref bool Cancel)
+        {
+            string newFilename = this.Application.ActiveDocument.FullName;
+
+            newFilename = newFilename.Replace(".docx", ".pdf");
+          
+            this.Application.ActiveDocument.ExportAsFixedFormat(newFilename, WdExportFormat.wdExportFormatPDF);
+        }
+
+
+
+        #region Von VSTO generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+}
